e39b27ae608e747502662dc21d9167609f391c89
MarlonHDC <marlon_hdc@hotmail.com>
2024-10-23 11:14:13 -0300

    implementação concluída

--- index.js
+++ index.js
@@ -3,9 +3,29 @@ class hero{
         this.nome = nome
         this.idade = idade
         this.tipo = tipo
-    }    
+    }          
+
+    atacar(){    
+        let ataque    
+        
+        if(this.tipo === "mago"){
+            ataque = "magia"
+        } else if ( this.tipo === "guerreiro"){
+            ataque = "espada"
+        } else if (this.tipo === "monge"){
+            ataque = "artes marciais"
+        } else if (this.tipo === "ninja") {
+            ataque = "shuriken"
+        }      
+        
+        console.log(`O ${this.tipo} atacou usando ${ataque}`)        
+    }
 }
 
 let ninja = new hero ("Dev May Cry", 20, "ninja")
 let guerreiro = new hero ("Kratos Dev", 32, "guerreiro")
 let mago = new hero ("Gandaldev Senior", 2000, "mago")
+
+mago.atacar()
+guerreiro.atacar()
+ninja.atacar()
