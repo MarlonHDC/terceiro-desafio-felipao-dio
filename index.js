e39963f27805e20d95d6294309b7bda70aab5482
MarlonHDC <marlon_hdc@hotmail.com>
2024-10-23 09:20:35 -0300

    criando clase e objetos

---
+++ index.js
@@ -0,0 +1,11 @@
+class hero{
+    constructor(nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }    
+}
+
+let ninja = new hero ("Dev May Cry", 20, "ninja")
+let guerreiro = new hero ("Kratos Dev", 32, "guerreiro")
+let mago = new hero ("Gandaldev Senior", 2000, "mago")
